acc498e3d26ce5b52bdd34d5ca32068edbc916f1
himazin331 <sion20022013@gmail.com>
2021-04-26 06:41:39 +0900

    Feature map visualization module

--- feature_visual.py
+++ feature_visual.py
@@ -1,22 +1,22 @@
-import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' # TFメッセージ非表示
-
 import tensorflow as tf
 
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # TFメッセージ非表示
+
 
 # 特徴マップ可視化
 def feature_vi(model, input_shape, test_img):
-        
     # モデル再構築
     x = tf.keras.Input(shape=input_shape)
     model_vi = tf.keras.Model(inputs=x, outputs=model.call(x))
-     
+
     # ネットワーク構成出力
     model_vi.summary()
     print("")
-        
+
     # レイヤー情報を取得
     feature_vi = []
     feature_vi.append(model_vi.get_layer('input_1'))
@@ -30,24 +30,23 @@ def feature_vi(model, input_shape, test_img):
     img = test_img[idx]
     img = img[None, :, :, :]
 
-    for i in range(len(feature_vi)-1):
-            
+    for i in range(len(feature_vi) - 1):
         # 特徴マップ取得
-        feature_model = tf.keras.Model(inputs=feature_vi[0].input, outputs=feature_vi[i+1].output)
+        feature_model = tf.keras.Model(inputs=feature_vi[0].input, outputs=feature_vi[i + 1].output)
         feature_map = feature_model.predict(img)
         feature_map = feature_map[0]
         feature = feature_map.shape[2]
-            
+
         # ウィンドウ名定義
         fig = plt.gcf()
-        fig.canvas.set_window_title(feature_vi[i+1].name + " feature-map visualization")
-            
+        fig.canvas.set_window_title(feature_vi[i + 1].name + " feature-map visualization")
+
         # 出力
         for j in range(feature):
             plt.subplots_adjust(wspace=0.4, hspace=0.8)
-            plt.subplot(feature/6 + 1, 6, j+1)
+            plt.subplot(feature / 6 + 1, 6, j + 1)
             plt.xticks([])
             plt.yticks([])
             plt.xlabel(f'filter {j}')
-            plt.imshow(feature_map[:,:,j])
+            plt.imshow(feature_map[:, :, j])
         plt.show()
